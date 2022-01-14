bfa609ec6f3651d4380e4b049c907aa20f0f5a54
Nagaraj <nagaraj2794@rediffmail.com>
2022-01-14 19:13:51 +0530

    updated

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.use(fileMiddleware);
 
 require('dotenv').config({ path: __dirname + '/.env' });
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 // Logger
 if (process.env.ENV === "DEV") {
     // console.log("Will Start Logging");
