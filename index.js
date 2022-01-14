4c15d1cdf55ecccdad51da1189bd0855b3bf87c6
Nagaraj <nagaraj2794@rediffmail.com>
2022-01-14 15:23:22 +0530

    CORS

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const fileMiddleware = require('express-multipart-file-parser');
 const app = express();
 app.use(express.json({limit: '200mb'}));
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+
 // app.use(cors());
 app.use(cors({
     origin: "*",
