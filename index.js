ff4f1893dfcd32f2f9e008b8e65ecbca2756baba
Nagaraj <nagaraj2794@rediffmail.com>
2022-01-14 16:33:12 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json({limit: '200mb'}));
 
 
 
-app.use(cors());
+// app.use(cors());
 // app.use(cors({
 //     origin: "*",
 //     "methods": "GET,PUT,POST",
@@ -15,6 +15,31 @@ app.use(cors());
 //     "optionsSuccessStatus": 204,
 //     credentials: true
 // }));
+const allowedOrigins = [
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:8100',
+  ];
+  
+  // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Origin not allowed by CORS'));
+      }
+    },
+  };
+  
+  // Enable preflight requests for all routes
+  app.options('*', cors(corsOptions));
+  
+  app.get('/', cors(corsOptions), (req, res, next) => {
+    res.json({ message: 'This route is CORS-enabled for an allowed origin.' });
+  });
 app.use(fileMiddleware);
 app.use(function (req, res, next) {
     //Enabling CORS
